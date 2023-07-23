3d12e0407c3b8cd6f6d0e507374bb329b738f08e
Jace Manshadi <jmanshad@protonmail.com>
2023-07-23 11:23:31 -0700

    changes

--- jobs/management/commands/update_lists.py
+++ jobs/management/commands/update_lists.py
@@ -10,9 +10,12 @@ class Command(BaseCommand):
         applied_list, new = List.objects.all().get_or_create(name="Applied", user_id=1)
         hidden_list, new = List.objects.all().get_or_create(name="Hidden", user_id=1)
         for user_job_posting in user_job_postings:
+            log = []
             if user_job_posting.applied:
+                log.append("job saved to apply")
                 Item(list=applied_list, job=user_job_posting.job_posting).save()
             if user_job_posting.hide:
+                log.append("job saved to hidden")
                 Item(list=hidden_list, job=user_job_posting.job_posting).save()
             if user_job_posting.note is None or len(user_job_posting.note.strip()) == 0:
                 user_job_posting.delete()
