dc49cf37196495cbd7f2fd405e6c0f22fc371743
Jace Manshadi <jmanshad@protonmail.com>
2023-07-23 01:53:58 -0700

    more changes

--- jobs/views.py
+++ jobs/views.py
@@ -160,11 +160,8 @@ class ListSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        request.data['user'] = request.user.id
+        return super(ListSet, self).create(request, args, kwargs)
 
     def get_queryset(self):
         request = self.request
