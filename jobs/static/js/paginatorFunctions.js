1374482d3c64995f26403234318fb8777e927697
Jace Manshadi <jmanshad@protonmail.com>
2023-07-24 21:40:43 -0700

    fixing bug where allLists was not a JSON

--- jobs/static/js/paginatorFunctions.js
+++ jobs/static/js/paginatorFunctions.js
@@ -1,27 +1,17 @@
-async function goToPreviousPage() {
-    const allLists = $.ajax({
-        'url': `${getCookie('list_endpoint')}`,
-        'type': 'GET',
-        'cache': false,
-        headers: {'X-CSRFToken': getCookie('csrftoken')},
-        contentType: 'application/json; charset=utf-8',
-        async: false
-    })
-    const view = getCookie("view");
-    const listObjectId = view.match(/^list_index_\d*$/) ? Number(view.slice(11)) : view;
-    await goToPage(allLists,  - 1, listObjectId);
-}
-async function goToNextPage() {
-    const allLists = $.ajax({
-        'url': `${getCookie('list_endpoint')}`,
-        'type': 'GET',
-        'cache': false,
-        headers: {'X-CSRFToken': getCookie('csrftoken')},
-        contentType: 'application/json; charset=utf-8',
-        async: false
-    })
-    const view = getCookie("view");
-    const listObjectId = view.match(/^list_index_\d*$/) ? Number(view.slice(11)) : view;
-    await goToPage(allLists, 1, listObjectId);
-}
+async function goToPreviousPage() {await goToSpecifiedPage(-1);}
 
+async function goToNextPage() {await goToSpecifiedPage(1);}
+
+async function goToSpecifiedPage(newPageDifference) {
+    const allLists = $.ajax({
+        'url': `${getCookie('list_endpoint')}`,
+        'type': 'GET',
+        'cache': false,
+        headers: {'X-CSRFToken': getCookie('csrftoken')},
+        contentType: 'application/json; charset=utf-8',
+        async: false
+    })
+    const view = getCookie("view");
+    const listObjectId = view.match(/^list_index_\d*$/) ? Number(view.slice(11)) : view;
+    await goToPage(allLists, newPageDifference, listObjectId);
+}
