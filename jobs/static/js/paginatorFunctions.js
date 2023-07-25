b576e9488bf84125677731ee2339cd73e7c57215
Jace Manshadi <jmanshad@protonmail.com>
2023-07-24 21:47:39 -0700

    fixing bug where allLists was not a JSON

--- jobs/static/js/paginatorFunctions.js
+++ jobs/static/js/paginatorFunctions.js
@@ -3,14 +3,14 @@ async function goToPreviousPage() {await goToSpecifiedPage(-1);}
 async function goToNextPage() {await goToSpecifiedPage(1);}
 
 async function goToSpecifiedPage(newPageDifference) {
-    const allLists = $.ajax({
+    const allLists = JSON.parse($.ajax({
         'url': `${getCookie('list_endpoint')}`,
         'type': 'GET',
         'cache': false,
         headers: {'X-CSRFToken': getCookie('csrftoken')},
         contentType: 'application/json; charset=utf-8',
         async: false
-    })
+    }).responseText)
     const view = getCookie("view");
     const listObjectId = view.match(/^list_index_\d*$/) ? Number(view.slice(11)) : view;
     await goToPage(allLists, newPageDifference, listObjectId);
