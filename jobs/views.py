7e16cc01795ff54b2a50abc5c8c6a284b93b45e7
Jace Manshadi <jaymanshad@proton.me>
2023-09-27 00:39:55 -0700

    updating logic so that there's no need for a second loop

--- jobs/views.py
+++ jobs/views.py
@@ -64,10 +64,18 @@ class JobsAppliedNumbers(View):
     def get(self, request):
         applied_jobs = Item.objects.all().filter(list__name='Applied').order_by('-date_added')
         applied_stats = {}
-        for applied_job in applied_jobs:
+        index = 0
+        last_date = None
+        number_of_dates = 0
+        while number_of_dates < 3:
+            applied_job = applied_jobs[index]
+            index+=1
             if applied_job.date_added is not None:
                 easy_apply = applied_job.job.easy_apply
                 date_str = applied_job.date_added.strftime("%Y-%m-%d")
+                if last_date != date_str:
+                    number_of_dates += 1
+                    last_date = date_str
                 if date_str not in applied_stats:
                     applied_stats[date_str] = {
                         'easy_apply' : 1 if easy_apply else 0,
@@ -75,13 +83,7 @@ class JobsAppliedNumbers(View):
                     }
                 else:
                     applied_stats[date_str]['easy_apply' if easy_apply else 'company_website_apply'] += 1
-        final_applied_stats = {}
-        index = 0
-        for (key, value) in applied_stats.items():
-            if index < 3:
-                final_applied_stats[key] = value
-                index += 1
-        return HttpResponse(json.dumps(final_applied_stats))
+        return HttpResponse(json.dumps(applied_stats))
 
 
 class JobSerializer(serializers.ModelSerializer):
