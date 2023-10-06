d269c764c142823c09570850045a8c3eb499da31
Jace Manshadi <jaymanshad@proton.me>
2023-10-06 10:17:28 -0700

    adding script to fix the job locations

---
+++ jobs/management/commands/parse_fixed_job_locations.py
@@ -0,0 +1,41 @@
+import csv
+import datetime
+import os
+
+from dateutil.tz import tz
+from django.core.management import BaseCommand
+
+from jobs.models import JobLocation, ETLFile
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        csv_files = ETLFile.objects.all()
+        if len(csv_files) != 1:
+            print(f"got an unexpected number of csv files [{len(csv_files)}]")
+            return
+        csv_file = csv_files[0]
+        if os.path.exists(csv_file.file_path):
+            with open(csv_file.file_path, 'r') as linkedin_export:
+                print(f"parsing {csv_file.file_path}")
+                csvFile = [line for line in csv.reader(linkedin_export)]
+                for line in csvFile[1:]:
+                    linkedin_id = line[0]
+                    job_location = JobLocation.objects.get(id=linkedin_id)
+                    experience_level = line[1]
+                    location = line[2]
+                    date_posted = line[3]
+                    if experience_level != "":
+                        job_location.experience_level = int(experience_level)
+                    if location != "":
+                        job_location.location = location
+                    else:
+                        print(1)
+                    if f"{date_posted}".isdigit():
+                        job_location.date_posted = datetime.datetime.fromtimestamp(
+                            int(date_posted) // 1000
+                        ).replace(microsecond=int(date_posted) % 1000 * 10).astimezone(
+                            tz.gettz('Canada/Pacific')
+                        )
+                    job_location.save()
