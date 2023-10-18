515685c657491e730298bdd24c43adf835e21b1c
Jace Manshadi <jaymanshad@proton.me>
2023-10-18 12:21:52 -0700

    fixing script so it ony looks at job locations that exist

--- jobs/management/commands/fix_timestamps.py
+++ jobs/management/commands/fix_timestamps.py
@@ -27,15 +27,27 @@ def parse_csv_export(file_path):
     with open(file_path, 'r') as linkedin_export:
         print(f"parsing {file_path}")
         csvFile = [line for line in csv.reader(linkedin_export)]
-        number_of_lines = len(csvFile) - 1
-        for index, line in enumerate(csvFile[1:]):
-            try:
-                job_location = JobLocation.objects.get(id=line[0])
-                job_location.joblocationdateposted_set.all().delete()
+        updated_times = {
+            line[0]: line[1]
+            for line in csvFile[1:]
+        }
+        job_locations = JobLocation.objects.all().exclude(job_board='Indeed')
+        number_of_jobs = len(job_locations)
+        for index, job_location in enumerate(job_locations):
+            job_location.joblocationdateposted_set.all().delete()
+            if job_location.id in updated_times:
                 JobLocationDatePosted(
                     job_location_posting=job_location,
-                    date_posted=pstdatetime.from_epoch(int(line[1]))
+                    date_posted=pstdatetime.from_epoch(int(updated_times[job_location.id]))
                 ).save()
-                print(f"parsed job {index}/{number_of_lines}")
-            except Exception as e:
-                print(f"error {e} {line[0]} - {index}/{number_of_lines}")
+                print(f"processed job {job_location.id} {index}/{number_of_jobs}")
+            elif job_location.date_posted is not None:
+                JobLocationDatePosted(
+                    job_location_posting=job_location,
+                    date_posted=job_location.date_posted
+                ).save()
+                print(f"processed job {job_location.id} {index}/{number_of_jobs}")
+            else:
+                print(f"skipping job {job_location.id} {index}/{number_of_jobs}")
+
+
