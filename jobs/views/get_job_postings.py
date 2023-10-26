f7049f3ae2bc48b517813d7a7bb894e853ac9abc
Jace Manshadi <jaymanshad@proton.me>
2023-10-26 09:17:21 -0700

    adding a flag so that for the time being, senior jobs aren't shown since I am not applying to them anyways and therefore it's just a waste of bandwidth atm

--- jobs/views/get_job_postings.py
+++ jobs/views/get_job_postings.py
@@ -2,6 +2,8 @@ from django.db.models import F, Case, When, Q
 
 from jobs.models import JobItem, ExperienceLevel, Job, JobLocationDatePostedItem
 
+SHOW_SENIOR_JOBS = False
+
 
 def get_job_postings(job_postings, user_id, list_parameter=None):
     if list_parameter is None or list_parameter == "all":
@@ -54,37 +56,38 @@ def get_job_postings(job_postings, user_id, list_parameter=None):
                 )
             pk_list.append(ordered_below_mid_senior_level_job_posting.id)
 
-    above_associate_level_jobs = job_postings.filter(
-        joblocation__experience_level__gt=ExperienceLevel.get_associate_number()
-    )
-    ordered_above_associate_level_jobs_postings = above_associate_level_jobs.order_by(
-        F('joblocation__easy_apply').desc(nulls_last=True), F('joblocation__joblocationdateposted__date_posted').desc(nulls_last=True),
-        F('joblocation__experience_level').desc(nulls_last=True),
-        'company_name', 'job_title', 'id'
-    )
-
-    easy_apply_ordered_above_associate_level_jobs_posting_pk_list = []
-    non_easy_apply_ordered_above_associate_level_jobs_posting_pk_list = []
-    for ordered_above_associate_level_jobs_posting in ordered_above_associate_level_jobs_postings:
-        if ordered_above_associate_level_jobs_posting.id not in pk_list:
-            if ordered_above_associate_level_jobs_posting.has_easy_apply and ordered_above_associate_level_jobs_posting.get_latest_job_posted_date_obj().job_location_posting.easy_apply:
-                easy_apply_ordered_above_associate_level_jobs_posting_pk_list.append(
-                    ordered_above_associate_level_jobs_posting.id
-                )
-            else:
-                non_easy_apply_ordered_above_associate_level_jobs_posting_pk_list.append(
-                    ordered_above_associate_level_jobs_posting.id
-                )
-            pk_list.append(ordered_above_associate_level_jobs_posting.id)
-
-    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(pk_list)])
-    job_postings = Job.objects.all().filter(pk__in=pk_list).order_by(preserved)
-
     number_of_easy_apply_below_mid_senior_job_postings = len(easy_apply_ordered_below_mid_senior_level_job_posting_pk_list)
     number_of_non_easy_apply_below_mid_senior_job_postings = len(non_easy_apply_ordered_below_mid_senior_level_job_posting_pk_list)
 
-    number_of_easy_apply_above_associate_job_postings = len(easy_apply_ordered_above_associate_level_jobs_posting_pk_list)
-    number_of_non_easy_apply_above_associate_job_postings = len(non_easy_apply_ordered_above_associate_level_jobs_posting_pk_list)
+    number_of_easy_apply_above_associate_job_postings = 0
+    number_of_non_easy_apply_above_associate_job_postings = 0
+    if SHOW_SENIOR_JOBS:
+        above_associate_level_jobs = job_postings.filter(
+            joblocation__experience_level__gt=ExperienceLevel.get_associate_number()
+        )
+        ordered_above_associate_level_jobs_postings = above_associate_level_jobs.order_by(
+            F('joblocation__easy_apply').desc(nulls_last=True), F('joblocation__joblocationdateposted__date_posted').desc(nulls_last=True),
+            F('joblocation__experience_level').desc(nulls_last=True),
+            'company_name', 'job_title', 'id'
+        )
+
+        easy_apply_ordered_above_associate_level_jobs_posting_pk_list = []
+        non_easy_apply_ordered_above_associate_level_jobs_posting_pk_list = []
+        for ordered_above_associate_level_jobs_posting in ordered_above_associate_level_jobs_postings:
+            if ordered_above_associate_level_jobs_posting.id not in pk_list:
+                if ordered_above_associate_level_jobs_posting.latest_job_posted_date_obj_is_easy_apply():
+                    easy_apply_ordered_above_associate_level_jobs_posting_pk_list.append(
+                        ordered_above_associate_level_jobs_posting.id
+                    )
+                else:
+                    non_easy_apply_ordered_above_associate_level_jobs_posting_pk_list.append(
+                        ordered_above_associate_level_jobs_posting.id
+                    )
+                pk_list.append(ordered_above_associate_level_jobs_posting.id)
+        number_of_easy_apply_above_associate_job_postings = len(
+            easy_apply_ordered_above_associate_level_jobs_posting_pk_list)
+        number_of_non_easy_apply_above_associate_job_postings = len(
+            non_easy_apply_ordered_above_associate_level_jobs_posting_pk_list)
 
     preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(pk_list)])
     job_postings = Job.objects.all().filter(pk__in=pk_list).order_by(preserved)
