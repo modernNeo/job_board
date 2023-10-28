b630a8eb9e60495e19dbd7490bfe794c4db21ca3
Jace Manshadi <jaymanshad@proton.me>
2023-10-27 20:34:24 -0700

    fixing search so it searches the selected list rather than search the inbox

--- jobs/static/js/searchJob.js
+++ jobs/static/js/searchJob.js
@@ -4,21 +4,31 @@ async function searchJob() {
     const search_company = document.getElementById("search_company").value.trim();
     if (search_title.length > 0) {
         setCookie("search_title", search_title);
-    }else{
+    } else {
         setCookie("search_title", null);
     }
     if (search_id.length > 0) {
         setCookie("search_id", search_id);
-    }else{
+    } else {
         setCookie("search_id", null);
     }
     if (search_company.length > 0) {
         setCookie("search_company", search_company);
-    }else{
+    } else {
         setCookie("search_company", null);
     }
-    await showInbox();
+    const allLists = JSON.parse($.ajax({
+        'url': `${getCookie('list_endpoint')}`,
+        'type': 'GET',
+        'cache': false,
+        headers: {'X-CSRFToken': getCookie('csrftoken')},
+        contentType: 'application/json; charset=utf-8',
+        async: false
+    }).responseText)
+    setCookie("previously_selected_job_index", getCookie("currently_selected_job_index"));
+    await refreshAfterJobOrListUpdate(allLists);
 }
+
 async function clearSearch() {
     document.getElementById("search_title").value = "";
     document.getElementById("search_id").value = "";
@@ -26,5 +36,14 @@ async function clearSearch() {
     setCookie("search_title", null);
     setCookie("search_id", null);
     setCookie("search_company", null);
-    await showInbox();
+    const allLists = JSON.parse($.ajax({
+        'url': `${getCookie('list_endpoint')}`,
+        'type': 'GET',
+        'cache': false,
+        headers: {'X-CSRFToken': getCookie('csrftoken')},
+        contentType: 'application/json; charset=utf-8',
+        async: false
+    }).responseText)
+    setCookie("previously_selected_job_index", getCookie("currently_selected_job_index"));
+    await refreshAfterJobOrListUpdate(allLists);
 }
